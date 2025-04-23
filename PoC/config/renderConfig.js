2e43db9d9300a9e0775dad2e731f31bc850779cb
NidhiIyer04 <nidhiiyer240604@gmail.com>
2025-04-23 21:34:30 +0530

    Organised repository

--- PoC/config/renderConfig.js
+++
@@ -1,9 +0,0 @@
-export const BASE_LAYER = 0
-export const BLOOM_LAYER = 1
-export const OVERLAY_LAYER = 2
-
-export const BLOOM_PARAMS = {
-    bloomThreshold: 0.5,
-    bloomStrength: 1.5,
-    bloomRadius: 0.4
-}
